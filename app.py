f8f18acbd9e28f8a1dddcedf55addad658491e9e
Yassine YADINE <yadine.yassine7@gmail.com>
2026-05-30 23:02:56 +0200

    refactor app entry point

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Configuration de la page (titre de l'onglet, icône, mise en page)
+st.set_page_config(
+    page_title="Municipality",
+    page_icon="🗺️",
+    layout="wide"
+)
+
+st.title("Municipality 🇫🇷")
+st.write("Cette application permettra de visualiser la répartition des élus en France.")
